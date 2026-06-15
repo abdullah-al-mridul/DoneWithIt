531997522673bd61b7cd038b7e1de429bc50825f
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-15 20:29:37 +0600

    dimensions

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,23 @@
 import {
-  Button,
+  Dimensions,
   Platform,
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  View,
 } from "react-native";
 
 export default function Index() {
+  console.log(Dimensions.get("screen"));
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Click me" />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "50%",
+          height: 70,
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
