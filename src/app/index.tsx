b12a967946acb61e8f439a5afb8360fce163725f
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-15 17:33:21 +0600

    views and native touch

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,29 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, TouchableNativeFeedback, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          height: 200,
-          width: 300,
-          uri: "https://picsum.photos/200/300.jpg",
+      <TouchableNativeFeedback
+        onPress={() => {
+          console.log("image tapped");
         }}
-      />
+      >
+        <View
+          style={{
+            width: 200,
+            height: 300,
+            backgroundColor: "red",
+          }}
+        >
+          <Image
+            source={{
+              height: 200,
+              width: 300,
+              uri: "https://picsum.photos/200/300.jpg",
+            }}
+          />
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
