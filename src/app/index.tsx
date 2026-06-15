e7f69a7e84d94ec796c11fb4793e4af724a50592
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-15 21:54:00 +0600

    update view dimensions and styles for layout consistency

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,20 +7,22 @@ export default function Index() {
         backgroundColor: "#fff",
         flex: 1,
         flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 100,
+          height: 300,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 100,
+          height: 200,
         }}
       />
       <View
