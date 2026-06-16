45d2638358aae44cadce9c424cd883832864d926
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-16 11:38:32 +0600

    text style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,23 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10,
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "600",
+          color: "tomato",
+          textTransform: "capitalize",
+          textAlign: "center",
+          lineHeight: 30,
         }}
       >
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 50,
-            height: 50,
-          }}
-        ></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          margin: 20,
-        }}
-      ></View>
+        I love react native! Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Quis, nobis ullam ratione amet nisi reiciendis similique
+        consectetur quidem vero! Aliquid.
+      </Text>
     </View>
   );
 };
