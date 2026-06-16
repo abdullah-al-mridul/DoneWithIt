1007840b6d23a0e3e2fd71cd09aa981c69eb3ae1
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-16 11:52:11 +0600

    refactor: replace inline text component with reusable AppText component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import AppText from "./components/AppText";
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontStyle: "italic",
-          fontWeight: "600",
-          color: "tomato",
-          textTransform: "capitalize",
-          textAlign: "center",
-          lineHeight: 30,
-        }}
-      >
-        I love react native! Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Quis, nobis ullam ratione amet nisi reiciendis similique
-        consectetur quidem vero! Aliquid.
-      </Text>
+      <AppText>I love react native!</AppText>
     </View>
   );
 };
