10d4c9fdf1e95c13a4ca79a364d82aefc67f22e5
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-19 12:50:54 +0600

    feat: enhance AppPicker component with modal functionality for improved user interaction

--- src/app/components/AppPicker.tsx
+++ src/app/components/AppPicker.tsx
@@ -1,32 +1,51 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import React from "react";
-import { StyleSheet, TextInputProps, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  Pressable,
+  StyleSheet,
+  TextInputProps,
+  View,
+} from "react-native";
 
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
+import Screen from "./Screen";
 
 interface AppTextInputType extends TextInputProps {
   icon?: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
 }
 
 const AppPicker = ({ icon, placeholder }: AppTextInputType) => {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          name={icon}
-          size={20}
-          color={defaultStyles.colors.medium}
-          style={styles.icon}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder} </AppText>
-      <MaterialCommunityIcons
-        name="chevron-down"
-        size={20}
-        color={defaultStyles.colors.medium}
-      />
-    </View>
+    <>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder} </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </Pressable>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 };
 
