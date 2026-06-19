94c8858e6b24f0c0eb625855169bc60474ca2179
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-19 18:56:17 +0600

    feat: refactor App component to use LoginScreen and remove unused code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,43 +1,7 @@
-import { useState } from "react";
-import AppPicker from "./components/AppPicker";
-import AppTextInput from "./components/AppTextInput";
-import Screen from "./components/Screen";
-
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-];
-
-type CategoryType = {
-  label: string;
-  value: number;
-};
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
-  const [category, setCategory] = useState<CategoryType>(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        items={categories}
-        placeholder="Category"
-        icon="apps"
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-      />
-      <AppTextInput placeholder="Email" icon="email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 };
 
 export default App;
