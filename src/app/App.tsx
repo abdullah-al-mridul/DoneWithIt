d748a89095aedda75a98791017f0c1280dc9d5ad
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-23 18:11:51 +0600

    feat: add expo-media-library dependency and implement image selection feature in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import * as ImagePicker from "expo-image-picker";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Image } from "react-native";
+import AppButton from "./components/Button";
 import Screen from "./components/Screen";
 const App = () => {
   const requestPermission = async () => {
@@ -9,7 +11,35 @@ const App = () => {
   useEffect(() => {
     requestPermission();
   }, []);
-  return <Screen></Screen>;
+
+  const [imageUri, setImageUri] = useState<ImagePicker.ImagePickerAsset[]>();
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) {
+        setImageUri(result.assets);
+        console.log(result.assets);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Screen>
+      <AppButton title="Select Image" onPress={selectImage} />
+      {imageUri && (
+        <Image
+          source={{
+            uri: imageUri[0].uri,
+          }}
+          style={{
+            height: 200,
+            width: 200,
+          }}
+        />
+      )}
+    </Screen>
+  );
 };
 
 export default App;
