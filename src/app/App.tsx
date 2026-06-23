2e64b644320267123918f93b244e725cd9013dc5
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-23 17:46:57 +0600

    feat: add expo-image-picker dependency and implement image permission request in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
-import ListingEditScreen from "./screens/ListingEditScreen";
-
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import Screen from "./components/Screen";
 const App = () => {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access images");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 };
 
 export default App;
